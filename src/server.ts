281f96c90bdbf0ec4d23deaee79c2fb4fe37a8b4
ViniciusLagoGehrke <46430040+ViniciusLagoGehrke@users.noreply.github.com>
2020-10-19 01:40:47 +0100

    fixed issues

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); //habilita JSON no express
 app.use(routes);
-app.use('/updloads', express.static(path.join(__dirname,'..', 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname,'..', 'uploads')))
 app.use(errorHandler);
 
 app.listen(3333); // localhost: 3333
